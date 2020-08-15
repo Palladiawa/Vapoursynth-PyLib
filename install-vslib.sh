6d0db1e83e3c4d8cce2a309d3ac967c6e678a7cf
Palladiawa <875652873@qq.com>
2020-08-15 13:48:56 +0800

    update some script and submodules

---
+++ install-vslib.sh
@@ -0,0 +1,9 @@
+dir=vslib
+mkdir $dir
+cp	"./adjust/adjust.py"						"./$dir/"
+cp	"./CSMOD/CSMOD.py"							"./$dir/"
+cp	"./havsfunc/havsfunc.py"					"./$dir/"
+cp	"./mvsfunc/mvsfunc.py"						"./$dir/"
+cp	"./nnedi3_resample/nnedi3_resample.py"		"./$dir/"
+cp	"./pmsfunc/pmsfunc.py"						"./$dir/"
+cp	"./vsTAAmbk/vsTAAmbk.py"					"./$dir/"
---
+++ loadlib.py
@@ -0,0 +1,6 @@
+from CSMOD				import CSMOD
+from havsfunc			import havsfunc 		as haf 
+from mvsfunc			import mvsfunc 			as mvf 
+from nnedi3_resample	import nnedi3_resample	as nnrs 
+from vsTAAmbk			import vsTAAmbk			as taa 
+from pmsfunc			import pmsfunc			as pmf 
